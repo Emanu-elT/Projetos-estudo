cc7c8cbe7fee7f3b768890c9eeed22e994451187
Emanu-el <emanueltiago.silva@gmail.com>
2023-05-04 03:39:59 -0400

    teste 2

--- Teste 1/src/App.java
+++ Teste 1/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int numero=-10;
+        int numero=10;
         String resultado="";
         if(numero>=0){
             resultado="positivo";
